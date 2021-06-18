dbada0194de6a1fef2b04bd68c7d8cd04a3c68ea
Arinpe <dehtolah@gmail.com>
2021-06-18 11:09:13 +0100

    Test for errors

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -11,13 +11,24 @@ RSpec.describe Friendship, type: :model do
   end
 
   describe 'Friendship confirmed' do
+    before :each do
+      @user = User.create(id: 2, name: 'first', email: 'test@gmail.com', password: '1236476',
+                          password_confirmation: '1236476')
+      @friend = User.create(id: 1, name: 'second', email: 'testo@gmail.com', password: '1235679',
+                            password_confirmation: '1235679')
+    end
+    after :each do
+      @user.destroy
+      @friend.destroy
+    end
     it {
-      user = User.create(id: 2, name: 'first', email: 'test@gmail.com', password: '1236476',
-                         password_confirmation: '1236476')
-      friend = User.create(id: 1, name: 'second', email: 'testo@gmail.com', password: '1235679',
-                           password_confirmation: '1235679')
-      friendship = Friendship.create(user_id: user.id, friend_id: friend.id, confirmed: true)
+      friendship = Friendship.create(user_id: @user.id, friend_id: @friend.id, confirmed: true)
       expect(friendship.confirmed).to be true
+    }
+
+    it {
+      Friendship.create(user_id: @user.id, friend_id: @friend.id, confirmed: true)
+      expect(@user.friends.first.name).to eq 'first'
     }
   end
 end
