a17e758640cb6455ef89fb631b466debae724ff4
themonster2015 <quynhyen.vothi@gmail.com>
2021-11-02 10:59:21 +0100

    added registration controller

---
+++ app/controllers/api/v1/registration_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::RegistrationController < ApiController
+    skip_before_action :authenticate_user!
+    def create
+      @user = User.new(name: params[:name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+      if @user.save
+        render json: { token: JsonWebToken.encode(sub: @user.id) }
+      else
+        render json: { errors: ['Registration failed'] }
+      end
+    end
+  end
