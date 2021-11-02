9da3c19bfe5c4e37d9d92b2673fe0c758f1f5d1d
unknown <safaaballagh97@gmail.com>
2021-11-02 10:57:17 +0100

    Create route for registration

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post :auth, to: "authentication#create"
+      post :register, to: "registration#create"
       resources :posts, only: [:index, :create] do
         resources :comments, only: [:create, :index]
       end
